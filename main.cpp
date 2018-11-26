6e9da6762ea1e041cdf55b9534c0b6de98874ea4
sschatz1997 <s.schatz1997@gmail.com>
2018-11-26 18:46:51 -0500

    main C++

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	system("PAUSE");
+	return 0;
+}
